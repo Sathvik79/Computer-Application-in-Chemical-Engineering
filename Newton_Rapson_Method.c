3b71163d0b3500990f1d6d499028a0580f09d5a3
Sathvik79 <sathvikjain500@gmail.com>
2021-01-24 18:17:08 +0530

    Update Newton_Rapson_Method.c

--- Newton_Rapson_Method.c
+++ Newton_Rapson_Method.c
@@ -1,42 +1,42 @@
-//Newton Rapson Method
+// Newton Rapson Method:
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 void main(){
-    double p, t, r, mavg, acc, x1, x2, a1, b1, a2, b2;
-    double a12, am, bm, vg, fv, fdv, vnew, sv; 
+    float p, t, r, mavg, acc, x1, x2, a1, a2, b1, b2;
+    float a12, am, bm, vg, vnew, fv, fdv, sv;
     int noi, i, toi;
     printf("Enter Pressure and Temperature: ");
-    scanf("%lf %lf \n", &p, &t);
-    printf("Enter Univesal Gas Const. based on the Pressure and Temperature: ");
-    scanf("%lf \n", &r);
-    printf("Enter Avg. Molecuar Weight of Mixture: ");
-    scanf("%lf \n", &mavg);
-    printf("Enter Number of Iteration and Accuracy Level: ");
-    scanf("%d %lf \n", &noi, &acc);
-    printf("Enter mole fraction of two lquids: ");
-    scanf("%lf %lf \n", &x1, &x2);
-    printf("Enter constants a & b of liquid_1: ");
-    scanf("%lf %lf \n", &a1, &b1);
-    printf("Enter constants a & b of liquid_2: ");
-    scanf("%lf %lf \n", &a2, &b2);
+    scanf("%f %f", &p, &t);
+    printf("Enter Univesal Gas Constant for Temperature: ");
+    scanf("%f", &r);
+    printf("Enter Avgerage Molecular weight og the given Mixture: ");
+    scanf("%f", &mavg);
+    printf("Enter total Number of Iteration and Accuracy Level: ");
+    scanf("%d %f", &noi, &acc);
+    printf("Enter Mole Fraction of CO2 and n-Butane: ");
+    scanf("%f %f", &x1, &x2);
+    printf("Enter Molar Constant a & b of CO2: ");
+    scanf("%f %f", &a1, &b1);
+    printf("Enter Molar Constant a & b of n-Buatne: ");
+    scanf("%f %f", &a2, &b2);
     a12 = sqrt(a1*a2);
     am = (x1*x1*a1)+(2*x1*x2*a12)+(x2*x2*a2);
     bm = (x1*b1)+(x2*b2);
-    printf("am: %lf \t bm: %lf \n", am, bm);
+    printf("am: %f \t bm: %f", am, bm);
     vg = (r*t)/p;
-    printf("Molar Volume by Ideal Gas: %lf \n", vg);
-    for (i=0; i<=noi; i++){
-        fv = p*pow(vg, 3)-(pow(vg, 2)*(p*bm+r*t))+(vg*am)-(am*bm);
+    printf("\nThe Molar Volume by Ideal Gas Law: %f", vg);
+    for (i=1; i<=noi; i++){
+        fv = p*pow(vg, 3)-pow(vg, 2)*(p*bm+r*t)+(vg*am)-(am*bm);
         fdv = 3*p*pow(vg, 2)-2*vg*(p*bm+r*t)+am;
         vnew = vg-(fv/fdv);
-        printf("Trail[%d] = %lf", i, vnew);
+        printf("\nTrail [%d]: %f", i, vnew);
         toi = fabs(vnew-vg)/vg;
         if(toi<acc){
-            printf("Molar Volume: %lf", vnew);
+            printf("\nMolar Volume: %f", vnew);
             sv = vnew/mavg;
-            printf("Specific Volume: %lf", sv);
+            printf("\nSpecific Volume: %f", sv);
             exit(0);
         }
         else
