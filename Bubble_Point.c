7e57089ab45437fde8bc4b7eaf010748c7469e7e
Sathvik79 <sathvikjain500@gmail.com>
2021-01-25 04:29:48 +0530

    Update Bubble_Point.c

--- Bubble_Point.c
+++ Bubble_Point.c
@@ -1,47 +1,51 @@
 // Bubble Point for Multi-Component Mixture
 
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 void main(){
-	float P, t, acc, sumf, sumfd;
-	float a[10], b[10], c[10], x[10], bp[10], y[10];
-	int n, noi;
-	printf("Enter the number of components: ");
-	scanf("%d", &n);
-	printf("Enter the Pressure of the System: ");
-	printf("%f", &P);
-	for (i=1; i<=n; i++){
-		printf("Enter the Values of Antonie Constant %d: ", i);
-		scanf("%f %f %f", &a[i], &b[i], &c[i]);
-		printf("Enter the Liquid Mole Fraction of Component %d: ", i);
-		scanf("%f", &x[i]);
-		printf("Enter the Boiling Point of Component: %d", i);
-		scanf("%f", &bp[i]);
+	float A[10],B[10],C[10],x[10],y[10],bp[10];
+	float t,bt,acc,f,fd,p,sumf,sumfd;
+	int i,j,k,n,noi;
+	printf("Enter the number of components \n");
+	scanf("%d",&n);
+	printf("Enter the total pressure of the system \n");
+	scanf("%f",&p);
+	for(i=1;i<=n;i++)
+	{ 
+	printf("Enter the values of antione constants of component %d \n",i);
+	scanf("%f %f %f",&A[i],&B[i],&C[i]);
+	printf("Enter the liquid molefraction of component %d \n",i);
+	scanf("%f",&x[i]);
+	printf("Enter the boling point of component %d \n",i);
+	scanf("%f",&bp[i]);
 	}
-	t = 0;
-	for (i=0l i<=n; i++)
-		t = t+bp[i]*x[i];
-	printf("Enter the Accuracy Level and Number of Iteration: ");
-	scanf("%f %d", &acc, &noi);
-	for (j=1l j<=noi; j++){
-		sumf = sumfd = 0;
-		for (i=1; i<=n; i++){
-			f = exp(a[i]-(b[i]/(t+c[i]))))*x[i]/p;
-			fd = f*(b[i]/pow(t+c[i], 2));
-			sumf = sumf+f;
-			sumfd = sumfd+fd;
+    	t=0;
+	for(i=1;i<=n;i++){
+	 t=t+(bp[i]*x[i]);
+	}
+	printf("Enter the accuracy level and the number of iterations \n");
+	scanf("%f %d",&acc,&noi);
+	for(j=1;j<=noi;j++){
+		sumf=0.0;sumfd=0.0;
+		for(i=1;i<=n;i++){
+			f=exp(A[i]-(B[i]/(t+C[i])))*x[i]/p;
+			fd=f*(B[i]/pow(t+C[i],2));
+			sumf=sumf+f;
+			sumfd=sumfd+fd;
 		}
-		bt = t-((sumf-1)/sumfd);
-		printf("bt at the trail [%d]: %f", j, bt);
-		if (fabs(bt-t)/t<=acc){
-			printf("The Bubble Point Temperature %f: ", bt);
-			printf("The Bubble Composition: ");
-			for (k=1; k<=1; k++){
-				y[k] = exp(a[k]-(b[k]/(t+c[k])))*x[k]/p;
-				printf("y %d: %f", k, y[k]);
+		bt=t-((sumf-1)/sumfd);
+		printf("bt at trail [%d]=%f \n",j,bt);
+		if(fabs(bt-t)/t<=acc){
+			printf("The bubble point temperature is %f \n",bt);
+			printf("The bubble composition is \n");
+			for(k=1;k<=n;k++){
+				y[k]=exp(A[k]-(B[k]/(t+C[k])))*x[k]/p;
+				printf("y %d=%f \n",k,y[k]);
 			}
+                        exit(0);
 		}
-		t = bt;
+		t=bt;
 	}
-	printf("Number of Iteration is Not Sufficient");
+	printf("Number of iterations are not sufficient \n");
 }
